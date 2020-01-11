b496e56119db40a708048e44a06dd16981269fd0
RapidfireCRH <crhrapidfire@gmail.com>
2020-01-10 18:54:30 -0700

    trasfer to another machine

--- 100primes/Program.cs
+++ 100primes/Program.cs
@@ -47,93 +47,12 @@ namespace _100primes
                     }
                 }
             }
-            catch
+            catch (Exception e)
             {
-                dbclass.close();
+                Console.WriteLine("Error: " + e.Message);
+                Console.WriteLine("Stacktrace: " + e.StackTrace);
             }
             dbclass.close();
-        }
-        static bool isprime(string number)
-        {
-            if (number[number.Length - 1] != '1' && 
-                number[number.Length - 1] != '3' && 
-                number[number.Length - 1] != '7' && 
-                number[number.Length - 1] != '9')
-                return false;
-
-            string[] primes = dbclass.find_possible_factors(number);
-            foreach (string x in primes)
-            {
-                double num = double.Parse(x);
-                double num2 = double.Parse(number);
-                if (num * num > num2)//too big
-                    return true;
-                if (num2 % num == 0)//is divisable
-                    return false;
-            }
-
-            return true;
-        }
-        static void display(double num, DateTime timer)
-        {
-           uint addval(uint[] val, int num2 = -1)
-            {
-                uint ret = 0;
-                if (num2 == -1)
-                    num2 = val.Length;
-                for (int i = 0; i != num2; i++)
-                    ret += val[i];
-                return ret;
-            }
-            if (placeholder == 0)
-                placeholder = Double.Parse(dbclass.dbstats.databasesize);
-
-            Console.Clear();
-            Console.WriteLine("Process started at " + start.ToLongTimeString() + ".");
-            TimeSpan elapsedSpan = new TimeSpan(DateTime.Now.Ticks - start.Ticks);
-            Console.WriteLine("Running for {0:N0} days, {1} hours, {2} minutes, {3} seconds",
-                                elapsedSpan.Days, elapsedSpan.Hours,
-                                elapsedSpan.Minutes, elapsedSpan.Seconds);
-            Console.WriteLine("Currently running " + String.Format("{0:n0}", num));
-
-            if (DateTime.Now.Hour != timer.Hour)
-            {
-                for (int i = 22; i != -1; i--)
-                    hours[i + 1] = hours[i];
-                hours[0] = 0;
-            }
-            if (DateTime.Now.Minute != timer.Minute)
-            {
-                for (int i = 58; i != -1; i--)
-                    minutes[i + 1] = minutes[i];
-                minutes[0] = 0;
-            }
-            for (int i = 58; i != -1; i--)
-                seconds[i + 1] = seconds[i];
-
-            minutes[0] += seconds[0];
-            hours[0] += seconds[0];
-            double temp = Double.Parse(dbclass.dbstats.largestnum);
-            Console.Write("Last Prime Found: #" + String.Format("{0:n0}", Double.Parse(dbclass.dbstats.databasesize)) + " - ");
-            Console.WriteLine(String.Format("{0:n0}", temp));
-            double temp2 = (double)(seconds[0] + Double.Parse(dbclass.dbstats.databasesize));
-            Console.WriteLine(dbclass.dbstats.squarestoobig ? "We are now not able to calculate squares. This happened on prime number " + dbclass.dbstats.squarestoobigstart : "");
-
-
-            Console.WriteLine("");
-            Console.WriteLine("Found Prime Stats:");
-                Console.WriteLine(num - Double.Parse(prevnum) + " numbers processed.");
-                prevnum = num.ToString();
-            Console.WriteLine("Last Second      : " + seconds[0]);
-            Console.WriteLine("Last 5 Seconds   : " + addval(seconds, 5) + " (" + addval(seconds, 5) / 5 + ")");
-            Console.WriteLine("Last 30 Seconds  : " + addval(seconds, 30) + " (" + addval(seconds, 30) / 30 + ")");
-            Console.WriteLine("Last Minute      : " + addval(seconds) + " (" + addval(seconds) / 60 + ")");
-            Console.WriteLine("Last Hour        : " + addval(minutes) + " (" + (addval(minutes) / 60) / 60 + ")");
-            Console.WriteLine("Last 24 Hours    : " + addval(hours) + " (" + ((addval(hours) / 24) / 60) / 60 + ")");
-            placeholder = (double)(seconds[0] + Double.Parse(dbclass.dbstats.databasesize));
-
-
-            seconds[0] = 0;
         }
         
     }
---
+++ 100primes/net.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _100primes
+{
+    class _net
+    {
+        /*
+         * Storage (struct)
+         *      name, ip address, datestamp last heard, assigned from, assigned to, stacksize
+         *      
+         * Net packet expected:
+         *      Name, 1 (primes packet), [primes]
+         *      Name, 0, requested stack ammount
+         *      
+         * net listener
+         *      received -> verify IP vs name
+         *      if new, register name and ip, pass new assignment from top of stack with requested ammount, upto 200K
+         *      If existing and not prime packet, send existing entry in storage
+         *      if existing and prime packet, call db save command with stack, send new assignment with stacksize
+         *      
+         *  stack
+         *      stack of primes to send, in stacks of 20 odd numbers. populate stack when it gets below 20% of 500k
+         */
+    }
+    /// <summary>
+    /// Add
+    /// Read
+    /// 
+    /// </summary>
+    class _storage
+    {
+        public struct storestruct
+        {
+            public string name;
+            public IPAddress ip;
+            public DateTime timestamp;
+            public Double from;
+            public Double to;
+            public UInt64 stacksize;
+        }
+        private static List<storestruct> store;
+
+        private static bool _init = false;
+
+        public static void init()
+        {
+            //read from database
+            //populate store with each entry
+            store = new List<storestruct>();
+            _init = true;
+        }
+
+        /// <summary>
+        /// Add 
+        /// </summary>
+        /// <param name="entry"></param>
+        public static void add(storestruct entry)
+        {
+
+        }
+        
+        public static storestruct read(string name)
+        {
+            foreach (storestruct x in store)
+                if (x.name == name)
+                    return x;
+            return new storestruct();
+        }
+        public static storestruct read(double num)
+        {
+            foreach(storestruct x in store)
+                if (x.from < num && x.to > num)
+                    return x;
+            return new storestruct();
+        }
+
+        public static bool verify(string name, IPAddress ip)
+        {
+            if (!_init)
+                init();
+
+            foreach (storestruct x in store) 
+                if (x.name == name && x.ip.Equals(ip))
+                    return true;
+            return false;
+        }
+
+        
+        private static void load(string filename)
+        {
+            if (!_init)
+                init();
+        }
+
+        private static void save(string filename)
+        {
+            if (!_init)
+                init();
+        }
+    }
+}
